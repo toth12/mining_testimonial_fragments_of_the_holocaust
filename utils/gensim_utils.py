9b6f0568c2757c402971bfedeb35edca8bfb2b22
toth12 <gabor.toth@yale.edu>
2019-03-22 12:41:56 -0700

    Postprocessing function is ready

--- utils/gensim_utils.py
+++ utils/gensim_utils.py
@@ -120,130 +120,19 @@ def post_process_result_of_lda_topic_model(lda_model,gensim_corpus,document_coll
 			texts.append(document_collection[element])
 		#Append them to container
 		topic_closest_doc_with_topics_words.append({'texts':texts,'topic_words':all_topics[i]})
-
-	pdb.set_trace()
-
-
-
+	return {'topic_documents':topic_closest_doc_with_topics_words , 'document_topic_matrix' : document_topic_matrix}
+	
 
 def main():
-	#build dictionary
-	#todo: elimination of searched terms should happen later
-	'''results=blacklab.search_blacklab('<s/> <s/> (<s/> containing [lemma="naked" | lemma="undress" | lemma="strip"]) <s/> <s/>',window=0,lemma=True, include_match=True)
-	results=[match['complete_match'].strip() for match in results]
+	#todo: elimination of searched terms should happen later, eliminate stop words
+	document_collection=blacklab.search_blacklab('<s/> <s/> (<s/> containing [lemma="naked" | lemma="undress" | lemma="strip"]) <s/> <s/>',window=0,lemma=True, include_match=True)
+	document_collection=[match['complete_match'].strip() for match in document_collection]
+	result_lda_training = train_lda_topic_model_with_mallet(document_collection,constants.PATH_TO_MALLET,50)
 	
-	model = train_lda_topic_model_with_mallet(results,constants.PATH_TO_MALLET,50)
-	text.write_json('corpus', model['corpus'])
+	'''text.write_json('corpus', model['corpus'])
 	model['model'].save('ldamodel')
 	text.write_json('plain_texts', results)'''
 
-	post_process_result_of_lda_topic_model(LdaMallet.load('ldamodel'),text.read_json('corpus'),text.read_json('plain_texts'))
-	pdb.set_trace()
-	result = {'model': LdaMallet.load('ldamodel'),'corpus': text.read_json('corpus')}
-	plain_text = text.read_json('plain_texts')
-	lda_corpus = result['model'][result['corpus']]
-	all_topics = result['model'].show_topics(50)
-	
-	#prepare container
-	#create a container to keep the document topic matrix
-	document_topic_matrix=[]
-	#prepare a container for keeping the topic and the documents for which the given topic is the first one
-	topic_first_doc = {v:[] for v in range(0,50)}
-
-	#prepare a container for topics and the closest texts to them
-	topic_closest_doc = []
-
-	topic_closest_doc_with_topics_words = {}
-
-	#end of containers
-
-
-	for i,documents in enumerate(lda_corpus):
-		
-		#find the strongest topic for document
-		most_important_topic_for_document=np.array([elements[1] for elements in documents]).argmax()
-
-		
-
-		
-		#make a proper np array to keep the the document per topic relationship and add it to the document matrix
-		document_topic_matrix.append(np.array([elements[1] for elements in documents]))
-		
-		topic_first_doc[most_important_topic_for_document].append(plain_text[i])
-
-		'''
-
-		#add the id of the topic which is the strongest for a given document 
-		topics_list.append(topics)
-
-		#add the sentence and id to the topic which is the strongest
-		
-
-		'''
-		
-	#create the final document term matrix in numpy
-	document_topic_matrix=np.vstack(document_topic_matrix)
-
-	#Find the closest texts to a given topic
-	for i,element in enumerate(document_topic_matrix.T):
-		
-		closest=element.argsort(axis=0)[-15:][::-1]
-		texts = []
-		for element in closest:
-			texts.append(plain_text[element])
-		
-		topic_closest_doc.append({'texts':texts,'topic_words':all_topics[i]}) 
-
-
-		
-
-
+	post_processed = post_process_result_of_lda_topic_model(result_lda_training['model'],result_lda_training['corpus'],document_collection)
 	pdb.set_trace()
 	
-	#idaig
-
-	all_words = ''.join(results).split()
-	filtered_results = [word for word in all_words if ((word[0] in string.ascii_uppercase + string.ascii_lowercase))]
-    #todo: filter out stopwords	
-	dct=initialize_gensim_dictionary([filtered_results])
-	pdb.set_trace()
-	
-	print ('Adjectives')
-	print (i)
-	
-
-
-	train_lda_topic_model_with_mallet(contexts,'somepath')
-
-	'''
-	ids=text.read_json(constants.INPUT_FOLDER+'testimony_ids.json')
-	ids = [element['testimony_id'] for element in ids][0:16]
-	phrase_model=build_gensim_phrase_model_from_sentences(blacklab.iterable_results('<s/>',document_ids=ids,lemma=True))	
-	phrase_model.save(constants.OUTPUT_FOLDER+"phrase_model")
-
-	
-	model=build_gensim_synset_model_from_sentences(blacklab.iterable_results('<s/>',document_ids=ids,lemma=True, path_to_phrase_model=constants.OUTPUT_FOLDER+"phrase_model"))
-	cc=sorted(model.wv.vocab.keys())
-	model.save(constants.OUTPUT_FOLDER+'word2vecmodel')
-	print (len(cc))
-	pdb.set_trace()
-	
-	'''
-	'''
-	model=Word2Vec.build_vocab(vocabulary)
-
-
-
-	pdb.set_trace()
-	dct=initialize_gensim_dictionary(text_1+text_2)
-	corpus = [dct.doc2bow(text) for text in text_1+text_2]
-	word_freq_dic=[[(dct[id], freq) for id, freq in cp] for cp in corpus]
-	pdb.set_trace()
-	model=initialize_gensim_synset_model_with_dictionary(dct)
-	
-	dct=initialize_gensim_dictionary(text_1)
-	add_documents_to_gensim_dictionary(dct,text_2)
-	get_document_frequency_in_dictionary(dct)
-	print (dct.token2id)
-	pdb.set_trace()'''
-
